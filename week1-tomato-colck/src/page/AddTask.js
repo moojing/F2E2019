3f66471bfae0d8f0e6f77250ec7959a7af9c9816
Tsai Mujing <moojitsai@gmail.com>
2019-07-15 00:25:05 +0800

    finish add task

--- week1-tomato-colck/src/page/AddTask.js
+++ week1-tomato-colck/src/page/AddTask.js
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import Icon from '../component/Icon'
 let AddTask= ()=>{
 
     return (<>
-        <h1>
-            AddTask
-        </h1>
+        <div className="row px-5">
+            <div className="col-12"> 
+                <h1 className="title title-main py-4">ADD TASK</h1>
+            </div>
+            <div className="col-12 "> 
+                <h3 className="title title-sub py-2"> TASK TITLE </h3>
+                <input type="text" className="form-control" placeholder="My Second Task"/>
+            </div>
+            <div className="col-12 my-3 my-4"> 
+                <h3 className="title title-sub mb-3"> ESTIMATED TOMOTO </h3>
+               
+                <div className="d-flex justify-content-between">
+                {
+                    Array(10).fill(0).map((item,index)=>{
+                      return (  <Icon 
+                        key={index}
+                        name="TomatoSmall"
+                        className="icon-svg cursor-pointer"/>)
+                    }) 
+                  }
+                </div>
+            </div>
+            <div className="col-12 my-3"> 
+                <div className="btn btn-primary btn-block font-weight-bold ls-1">ADD TASK</div>
+            </div>
+        </div>
     </>)
 }
 
