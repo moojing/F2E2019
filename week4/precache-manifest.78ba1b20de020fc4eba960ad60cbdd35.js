42e816562b22725a3a5db0ac8217cb22125f9af4
Tsai Mujing <moojitsai@gmail.com>
2019-08-12 10:57:31 +0800

    Updates

---
+++ week4/precache-manifest.78ba1b20de020fc4eba960ad60cbdd35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d816b4d3f0313d088142f7bc4a980b4",
+    "url": "/F2E2019/week4/index.html"
+  },
+  {
+    "revision": "2acd164e9e5258263bcf",
+    "url": "/F2E2019/week4/static/css/main.3e19e842.chunk.css"
+  },
+  {
+    "revision": "d74f7242ac6a28720986",
+    "url": "/F2E2019/week4/static/js/2.a824c229.chunk.js"
+  },
+  {
+    "revision": "2acd164e9e5258263bcf",
+    "url": "/F2E2019/week4/static/js/main.ed9907b3.chunk.js"
+  },
+  {
+    "revision": "9d41b788edc2024234ac",
+    "url": "/F2E2019/week4/static/js/runtime~main.46110eed.js"
+  },
+  {
+    "revision": "b6736345d3710c42f0e3a5e088f69662",
+    "url": "/F2E2019/week4/static/media/ATM-light.b6736345.png"
+  },
+  {
+    "revision": "c4ed4c041a37fd33efb38c6873e8954a",
+    "url": "/F2E2019/week4/static/media/card-light.c4ed4c04.png"
+  },
+  {
+    "revision": "56e31c57ccf28484d2bd33b0d7110ad1",
+    "url": "/F2E2019/week4/static/media/shop-light.56e31c57.png"
+  }
+]);
