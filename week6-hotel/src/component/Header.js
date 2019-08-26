b0d0c8cc0a24bac8095384dada77804f7a8b27ba
Tsai Mujing <moojitsai@gmail.com>
2019-08-27 00:21:45 +0800

    [week6 ]add arrow

--- week6-hotel/src/component/Header.js
+++ week6-hotel/src/component/Header.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import {ReactComponent as Bed} from '../asset/bed.svg'
+import {ReactComponent as Date} from '../asset/date.svg'
+import {ReactComponent as ArrorRight} from '../asset/arror-right.svg'
 function Header() {
     return (
         <div className="header"> 
             <h1>HexHotel</h1>
             <ul className="list-group ">
                 <li className="list-group-item list-group-border">
-                    <Bed className="icon-svg"/>
+                    <Date className="icon-svg"/>
                     CHECK-IN
+                    <ArrorRight className="icon-arror-down"/>
+                </li>
+                <li className="list-group-item list-group-border">
+                    <Date className="icon-svg"/>
+                    CHECK-OUT
+                    <ArrorRight className="icon-arror-down"/>
                 </li>
                 <li className="list-group-item list-group-border">
                     <Bed className="icon-svg"/>
-                    CHECK-IN
-                </li>
-                <li className="list-group-item list-group-border">
-                    <Bed className="icon-svg"/>
-                    CHECK-IN
+                    ROOM
+                    <ArrorRight className="icon-arror-down"/>
                 </li>
                
             </ul>
