c44d838c368cce484be158f13ed53d7b9fcbcdff
Tsai Mujing <moojitsai@gmail.com>
2019-08-26 11:36:19 +0800

    [week6] add css reset

---
+++ week6-hotel/README.md
@@ -0,0 +1,5 @@
+## features
+
+## Mockup
+
+https://xd.adobe.com/spec/8384ecea-5384-44d2-72a7-94d9b883504d-f5b5/screen/f4fa511f-dc14-47d3-8f10-c8bba13418bb/room-total
---
+++ week6-hotel/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header"> 
+            <h1>HHotel</h1>
+            
+        </div>
+    );
+}
+
+export default Header 
