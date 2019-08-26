401b4ccd60e885558929dc6a910e7baa526bf33b
Tsai Mujing <moojitsai@gmail.com>
2019-08-26 15:13:30 +0800

    [week6] add checkin list

--- week6-hotel/src/component/Header.js
+++ week6-hotel/src/component/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import {ReactComponent as Bed} from '../asset/bed.svg'
 function Header() {
     return (
         <div className="header"> 
-            <h1>HHotel</h1>
-            
+            <h1>HexHotel</h1>
+            <ul className="list-group ">
+                <li className="list-group-item list-group-border">
+                    <Bed className="icon-svg"/>
+                    CHECK-IN
+                </li>
+                <li className="list-group-item list-group-border">
+                    <Bed className="icon-svg"/>
+                    CHECK-IN
+                </li>
+                <li className="list-group-item list-group-border">
+                    <Bed className="icon-svg"/>
+                    CHECK-IN
+                </li>
+               
+            </ul>
         </div>
     );
 }
