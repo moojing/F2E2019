9cc190479934c5f0c7bbe36f1ab90823a80f8674
Tsai Mujing <moojitsai@gmail.com>
2019-08-26 12:02:58 +0800

    Updates

--- week6/service-worker.js
+++ week6/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.355194935413c7b56c41200e250e30e3.js"
+  "/F2E2019/week6/precache-manifest.bc7579d7e43194a204c139ce377ac6ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/F2E2019/week6/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
